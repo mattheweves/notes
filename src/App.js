d01db2330854afcc3da70037afe7afea6ebed0bf
Matt Eves <mattheweves@outlook.com>
2017-05-24 08:13:02 -0700

    Add State for Note/List

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
-class App extends Component {
+class App extends Component{
+  constructor () {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
   render() {
+    const { showNote } = this.state;
+
     return (
       <div className="App">
         <Nav />
-        <List />
-        <Note />
+        { showNote ? <Note /> : <List />}
       </div>
     );
   }
