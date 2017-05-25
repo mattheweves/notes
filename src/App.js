a59091f92b6e7845335ba952d2a2e6bb292eab74
Matt Eves <mattheweves@outlook.com>
2017-05-24 21:30:07 -0700

    Note API Hooked up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component{
   constructor () {
@@ -13,13 +13,25 @@ class App extends Component{
     };
   }
 
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    });
+  }
+
+  getNotes = () => {
+    axios.get('https://notes-mattyboy.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote } = this.state;
 
     return (
       <div className="App">
-        <Nav />
-        { showNote ? <Note /> : <List />}
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        { showNote ? <Note /> : <List getNotes={this.getNotes}/>}
       </div>
     );
   }
